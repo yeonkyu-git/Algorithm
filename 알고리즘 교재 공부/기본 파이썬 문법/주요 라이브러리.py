2b08691be7deade3efae67ce4d007c375525fbf9
yeonkyu-git <yk0924.kim@gmail.com>
2021-02-06 14:43:31 +0900

    qwe

--- 알고리즘 교재 공부/기본 파이썬 문법/주요 라이브러리.py
+++ 알고리즘 교재 공부/기본 파이썬 문법/주요 라이브러리.py
@@ -61,3 +61,27 @@ print(result)
 
 
 ## (5) bisect
+# 이진 탐색을 쉽게 구현할 수 있음
+# 정렬된 배열에서 특정한 원소를 찾아야 할 때 매우 효과적으로 사용 됨
+# bisect_left()  bisect_right() 함수가 가장 중요하게 사용되며 시간 복잡도는 O(logN), 반환값은 삽입될 index
+# 정렬된 리스트에서 새롭게 데이터 4를 삽입하려할 때, 정렬을 유지하면서 삽입한다.
+from bisect import bisect_left, bisect_right
+
+a = [1, 2, 4, 4, 8]
+x = 4
+print(bisect_left(a,x))
+print(bisect_right(a,x))
+#정렬된 리스트에서 값이 특정 범위에 속하는 원소의 개수를 구하고자 할 때, 효과적
+
+def count_by_range(a, left_value, right_value):
+  right_index = bisect_right(a, right_value)
+  left_index = bisect_left(a, left_value)
+  return right_index - left_index
+
+a = [1, 2, 3, 3, 3, 3, 4, 4, 8, 9]
+
+# 값이 4인 데이터 개수 출력
+print(count_by_range(a, 4, 4))
+
+# 값이 [-1, 3] 범위에 있는 데이터 개수 출력
+print(count_by_range(a, -1, 3))
